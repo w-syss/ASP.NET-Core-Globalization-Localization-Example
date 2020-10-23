bdd552105fde4fc7f8e1e3f839e1dc74653779fb
Ole Wiedemann <61351036+w-syss@users.noreply.github.com>
2020-10-23 15:53:42 +0200

    Added localization for views. Added _SelectLanguagePartial view to select a language. Added controller operation to change languages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ASPNETCoreGlobalizationExample.Models;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Http;
 
 namespace ASPNETCoreGlobalizationExample.Controllers
 {
@@ -32,6 +34,18 @@ namespace ASPNETCoreGlobalizationExample.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
