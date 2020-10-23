7a0f89082ce2c05e08460c668879d706bc35e17d
Ole Wiedemann <61351036+w-syss@users.noreply.github.com>
2020-10-23 16:06:10 +0200

    Added sample model. Added sample form.

---
+++ Models/IndexModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreGlobalizationExample.Models
+{
+    public class IndexModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateInput { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal CurrencyInput { get; set; }
+    }
+}
